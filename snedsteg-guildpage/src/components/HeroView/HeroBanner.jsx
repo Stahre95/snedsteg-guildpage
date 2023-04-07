e6bd8f4498ddd6daaf08bd745fa12af1a689e4bc
Stahre95 <johan@bk30.se>
2023-04-07 11:43:26 +0200

    added HeroBanner and HeroText

--- snedsteg-guildpage/src/components/HeroView/HeroBanner.jsx
+++ snedsteg-guildpage/src/components/HeroView/HeroBanner.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../HeroView/HeroBanner.css'
 
+
+
 function HeroBanner() {
   return (
-    <div>
-      HeroBanner
+    <div className="hero-text-container">
+      <h3 className="hero-subtitle">KAZZAK</h3>
+      <h1 className="hero-title">SNEDSTEG</h1>
+      <p className="hero-paragraph">EN SVENSK GUILD FÖR ALLA</p>
     </div>
   );
 }
